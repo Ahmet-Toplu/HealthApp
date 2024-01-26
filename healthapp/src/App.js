4079beed417465eaf508d8a46c21a9aa211dea75
AlexHofman <alexxhhofman@gmail.com>
2024-01-26 18:49:32 +0000

    Adding comments

--- healthapp/src/App.js
+++ healthapp/src/App.js
@@ -1,11 +1,13 @@
+// Import the Navigation component from './Routes'
 import { Navigation } from './Routes';
 
+// Define the main App component
 export const App = () => {
-    return(
-        
-        // To run the function from ./Routes
+    return (
+        // Render the Navigation component, which includes your routes
         <Navigation />
     );
 }
 
+// Export the App component as the default export
 export default App;
