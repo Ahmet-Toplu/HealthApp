f4d1fe61d33e779be778b15734d15e070f187e0b
AlexHofman <alexxhhofman@gmail.com>
2024-02-25 15:27:15 +0000

    update

--- healthapp/frontend/src/pages/articles.js
+++ healthapp/frontend/src/pages/articles.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import '../css/contact.css';
 
 export const ArticlesPage = () => {
   const [articles, setArticles] = useState([]);
@@ -18,26 +19,28 @@ export const ArticlesPage = () => {
   };
 
   return (
-    <div>
-      <h1>Articles</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>URL</th>
-            <th>Title</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {articles.map((article) => (
-            <tr key={article.article_id}>
-              <td>{article.link}</td>
-              <td>{article.title}</td>
-              <td>{article.date}</td>
+    <div className='mx-3 mt-3'>
+      <div className='contact-container'>
+        <h1 className='contact-title'>Articles</h1>
+        <table className='contact-table'>
+          <thead>
+            <tr>
+              <th>URL</th>
+              <th>Title</th>
+              <th>Date</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {articles.map((article) => (
+              <tr key={article.article_id}>
+                <td>{article.link && <a href={article.link} target="_blank" rel="noopener noreferrer">{article.link}</a>}</td>
+                <td>{article.title}</td>
+                <td>{article.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
